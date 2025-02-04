165952278cc785058da138f672eac54dd204a567
Hitalo-Lima <hitalolima173@gmail.com>
2025-02-04 09:24:34 -0300

    feat: add ResponseEntity to handle responses returns when creating a new user

--- src/main/java/br/com/hitalo_lima/todolist/user/UserController.java
+++ src/main/java/br/com/hitalo_lima/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.hitalo_lima.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +17,15 @@ public class UserController {
 
     // Informações do POST vão vir por meio do body da requisição
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null) {
-            System.out.println("Usuário já existe na base de dados!");
-            return null;
+            System.out.printf("Usuário %s já existe na base de dados!", userModel.getUsername());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
